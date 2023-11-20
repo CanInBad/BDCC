c1c53d664e2fcfc67483c09a6913f1f4bc4423f8
CanInBad <user12335426@gmail.com>
2023-11-20 22:58:49 +0700

    mod

---
+++ Modules/PointBurnerCIB/Module.gd
@@ -0,0 +1,22 @@
+extends Module
+
+func _init():
+	id = "PointBurnerCIB"
+	author = "CanInBad"
+	
+	perks = [
+		"res://Modules/PointBurnerCIB/Perks/CumProduBuff1.gd",
+		"res://Modules/PointBurnerCIB/Perks/CumProduBuff2.gd",
+		"res://Modules/PointBurnerCIB/Perks/CumProduBuff3.gd",
+		"res://Modules/PointBurnerCIB/Perks/CumProduBuff4.gd",
+		"res://Modules/PointBurnerCIB/Perks/CriticalCombat.gd",
+		"res://Modules/PointBurnerCIB/Perks/CriticalLust.gd",
+	]
+
+	buffs = [
+		"res://Modules/PointBurnerCIB/Buffs/CumProductionBuff.gd",
+		"res://Modules/PointBurnerCIB/Buffs/CumPenisCapacityBuff.gd",
+		"res://Modules/PointBurnerCIB/Buffs/PhysicalDamageBuffRNG.gd",
+		"res://Modules/PointBurnerCIB/Buffs/LustDamageBuffRNG.gd",
+	]
+	# changed res://Player/Bodyparts/BodypartPenis.gd!!!!!
---
+++ Modules/PointBurnerCIB/Perks/CumProduBuff1.gd
@@ -0,0 +1,31 @@
+extends PerkBase
+
+func _init():
+	id = "Perk.CumProduBuff1"
+	skillGroup = Skill.CumLover
+
+func getVisibleName():
+	return "Hyperspermia Ⅰ"
+
+func getVisibleDescription():
+	return "Your balls fills up faster by 30%"
+
+func getMoreDescription():
+	return "You won't believe it, flat increase by 30%???? crazy."
+
+func getRequiredPerks():
+	return [Perk.CumStud, Perk.CumStudV2]
+
+func getSkillTier():
+	return 2
+
+func getCost():
+	return 4
+
+func getPicture():
+	return "res://Modules/PointBurnerCIB/Images/Perks/CumProduBuff1.png"
+
+func getBuffs():
+	return [
+		buff("Buff.CumPenisProductionBuff", [30])
+	]
