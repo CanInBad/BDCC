987fa46b756478a522d15ed07b7e0f6c7ce91b35
CanInBad <user12335426@gmail.com>
2023-11-21 15:29:13 +0700

    how?

--- Modules/PointBurnerCIB/Overwrite/Bodyparts/bob/HyperableBreasts.gd
+++ Modules/PointBurnerCIB/Overwrite/Bodyparts/bob/HyperableBreasts.gd
@@ -4,7 +4,7 @@ var breastSizeModClass = load("res://Modules/PointBurnerCIB/Overwrite/BreastsSiz
 func _init():
 	visibleName = "hyperable breasts"
 	id = "breastshyperable"
-	size = BreastsSize.C
+	size = breastSizeModClass.C
 
 # {FOREVER_FLAT = -1, FLAT = 0, A = 1, B = 2, C = 3, D = 4, DD = 5, E = 6, EE = 7, F = 8, FF = 9, G = 10, GG = 11}
 
