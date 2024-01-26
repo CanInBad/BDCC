ad4efe105ddbfcca318cb814f9a9e66245f6474d
CanInBad <73659199+CanInBad@users.noreply.github.com>
2024-01-26 20:21:27 +0700

    Added skins for hypertus stuff hypertus update

---
+++ Player/Player3D/SkinsPartsByAuthor/AverageAce/Penis/DragonPenisHyperable/DragonGradient.gd
@@ -0,0 +1,14 @@
+extends PartSkinBase
+
+func _init():
+	id = "Gradientdragonhyperable"
+	partID = "dragonpenishyperable"
+
+func getName():
+	return "Gradient"
+
+func getPatternTexture():
+	return {
+		"": preload("res://Player/Player3D/SkinsPartsByAuthor/AverageAce/Penis/DragonPenis/DragonGradient.png"),
+	}
+	
---
+++ Player/Player3D/SkinsPartsByAuthor/AverageAce/Penis/DragonPenisHyperable/DragonPiercing.gd
@@ -0,0 +1,15 @@
+extends PartSkinBase
+
+func _init():
+	id = "bodymoddragonhyperable"
+	partID = "dragonpenishyperable"
+
+func getName():
+	return "Pierced"
+
+func getPatternTexture():
+	return {
+		"": preload("res://Player/Player3D/SkinsPartsByAuthor/AverageAce/Penis/DragonPenis/Dragonpiercing.png"),
+	}
+	
+	 
