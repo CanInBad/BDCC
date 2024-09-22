ea3d7ec58af0bef44d5d7ce413ebb40886ed0fd4
CanInBad <73659199+CanInBad@users.noreply.github.com>
2024-09-22 21:31:03 +0700

    deletion of angled skin

--- Modules/0AnglarSkin/Module.gd
+++
@@ -1,7 +0,0 @@
-extends Module
-
-func _init():
-    id = "Angular Skin"
-    author = "\nConversion:\n  CanInBad\nSkin Maker:\n  AverageAce"
-    
-    GlobalRegistry.registerSkin("res://Modules/0AnglarSkin/theSkin/Angular.gd","AverageAce")
